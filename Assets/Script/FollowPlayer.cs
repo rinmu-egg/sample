81e49b36161885fb2a66e05e765b318b1cc000ac
rinmu-egg <rebirth.ia.rinmu@gmail.com>
2020-04-24 15:58:17 +0900

    サンプルデータ追加

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target; //ターゲットへの参照
+    private Vector3 offset;  //相対座標
+
+    void Start()
+    {
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position; 
+
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 8;
+
+    void FixedUpdate() 
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(x * speed, 0, z * speed);
+        
+    }
+}
